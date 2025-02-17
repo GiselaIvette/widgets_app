8d20d2bc5631c008a061dca5c6091a6899772e14
Gisela Garcia <gisegarcia2002@gmail.com>
2025-02-17 15:05:57 -0600

    screen creadas y barril actualizado

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -214,7 +214,7 @@ class _CardsType4 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
+    //final colors = Theme.of(context).colorScheme;
 
     return Card(
       clipBehavior: Clip.hardEdge,
